6a48d61efa054dce2b089a9ffca9f3ecd0e58131
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-04-09 17:21:05 +0200

    Implement recurring events (with plone.app.event dx type).

--- ftw/calendar/browser/calendarupdateview.py
+++ ftw/calendar/browser/calendarupdateview.py
@@ -1,4 +1,7 @@
 from DateTime import DateTime
+from plone.app.event.base import dates_for_display
+from plone.app.event.base import get_events
+from plone.app.event.base import RET_MODE_ACCESSORS
 from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 import json
@@ -20,7 +23,7 @@ class CalendarupdateView(BrowserView):
         mtool = getToolByName(self.context, 'portal_membership')
 
         self.result = []
-        self.cache = set()
+        self.cache = []
         self.memberid = mtool.getAuthenticatedMember().id
 
         query = {
@@ -30,6 +33,7 @@ class CalendarupdateView(BrowserView):
                 'query': DateTime(self.request.get('start')), 'range': 'min'}}
 
         self.get_data_without_recurrence(query)
+        self.get_data_with_recurrence(query)
 
         response = self.request.response
         response.setHeader('Content-Type', 'application/x-javascript')
@@ -50,9 +54,9 @@ class CalendarupdateView(BrowserView):
             )
 
         for brain in brains:
-            uid = brain.UID
-            if uid not in self.cache:
-                self.cache.add(uid)
+            url = brain.getURL()
+            if url not in self.cache:
+                self.cache.append(url)
                 self.result.append(self.format_brain(brain))
 
     def format_brain(self, brain):
@@ -69,6 +73,41 @@ class CalendarupdateView(BrowserView):
                 "className": "state-" + str(brain.review_state) +
                 (editable and " editable" or ""),
                 "description": brain.Description}
+
+    def get_data_with_recurrence(self, query):
+        start = query['start']
+        del query['start']
+        end = query['end']
+        del query['end']
+
+        query['path'] = {'depth': -1,
+                         'query': '/'.join(self.context.getPhysicalPath())}
+
+        occurencies = get_events(self.context,
+                                 start=start,
+                                 end=end,
+                                 expand=True,
+                                 ret_mode=RET_MODE_ACCESSORS,
+                                 **query)
+
+        for occ in occurencies:
+            url = occ.url
+            if url not in self.cache:
+                self.cache.append(url)
+                self.result.append(self.format_occurency(occ))
+
+    def format_occurency(self, occ):
+        dates = dates_for_display(occ)
+
+        return {"id": "UID_%s" % (occ.uid),
+                "title": occ.title,
+                "start": dates['start_iso'],
+                "end": dates['end_iso'],
+                "url": occ.url,
+                "editable": False,
+                "allDay": dates['whole_day'],
+                "className": "",
+                "description": occ.description}
 
 
 class CalendarDropView(BrowserView):
--- ftw/calendar/tests/test_calendar_update_view.py
+++ ftw/calendar/tests/test_calendar_update_view.py
@@ -1,8 +1,10 @@
 from DateTime import DateTime
+from datetime import datetime
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.calendar.browser.interfaces import IFtwCalendarLayer
 from ftw.calendar.testing import FTW_CALENDAR_FUNCTIONAL_TESTING
+from plone.app.event.dx.behaviors import IEventRecurrence
 from plone.app.event.interfaces import IBrowserLayer
 from plone.app.testing import login
 from plone.app.testing import setRoles
@@ -11,7 +13,6 @@ from plone.app.testing import TEST_USER_NAME
 from plone.uuid.interfaces import IUUID
 from unittest2 import TestCase
 from zope.interface import alsoProvides
-from datetime import datetime
 import json
 
 
@@ -82,3 +83,52 @@ class TestCalendarUpdateView(TestCase):
         self.assertEquals(
             expect,
             json.loads(folder.restrictedTraverse('@@ftwcalendar_update')())[0])
+
+    def test_result_using_dx_event_with_recurrence(self):
+        folder = create(Builder('folder'))
+        event = folder.get(
+            folder.invokeFactory(
+                'plone.app.event.dx.event',
+                'event1',
+                title=u'Event',
+                description=u'Description',
+                start=datetime(2000, 01, 01, 12, 0),
+                end=datetime(2000, 01, 01, 14, 0),
+                timezone=TZNAME,
+                whole_day=False
+            ))
+        event_w_rec = IEventRecurrence(event)
+        event_w_rec.recurrence = 'RRULE:FREQ=DAILY;COUNT=4'
+        event.reindexObject()
+
+        result = json.loads(
+            folder.restrictedTraverse('@@ftwcalendar_update')())
+
+        self.assertEquals(
+            4, len(result), 'Expect 4 items')
+
+        expect_event = {u'id': u'UID_{0}'.format(IUUID(event)),
+                        u'title': u'Event',
+                        u'start': DateTime(
+                            '2000/01/01 12:00:00 %s' % TZNAME).ISO8601(),
+                        u'end': DateTime(
+                            '2000/01/01 14:00:00 %s' % TZNAME).ISO8601(),
+                        u'url': event.absolute_url().decode('utf-8'),
+                        u'allDay': False,
+                        u'editable': True,
+                        u'className': u'state- editable',
+                        u'description': u'Description'}
+
+        self.assertEquals(expect_event, result[0])
+
+        expect_last_occ = {u'allDay': False,
+                           u'className': u'',
+                           u'description': 'Description',
+                           u'editable': False,
+                           u'end': u'2000-01-04T14:00:00+01:00',
+                           u'id': u'UID_{0}'.format(IUUID(event)),
+                           u'start': u'2000-01-04T12:00:00+01:00',
+                           u'title': u'Event',
+                           u'url': u'http://nohost/plone/folder/event1/2000-01-04'}
+
+        self.assertEquals(expect_last_occ, result[-1])
