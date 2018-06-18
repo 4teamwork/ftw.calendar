c4b81facf10f131c6c26c7a14009f941218e892b
Jonas Baumann <jone@jone.ch>
2018-06-18 14:54:55 +0200

    Tests: use dexterity builders for Plone 5.

--- ftw/calendar/tests/builders.py
+++ ftw/calendar/tests/builders.py
@@ -1,9 +1,22 @@
 from ftw.builder import builder_registry
 from ftw.builder.archetypes import ArchetypesBuilder
+from ftw.builder.dexterity import DexterityBuilder
+from Products.CMFPlone.utils import getFSVersionTuple
 
 
-class EventBuilder(ArchetypesBuilder):
-
+class EventBuilderMixin(object):
     portal_type = 'Event'
 
-builder_registry.register('event', EventBuilder)
+
+class ATEventBuilder(EventBuilderMixin, ArchetypesBuilder):
+    pass
+
+
+class DXEventBuilder(EventBuilderMixin, DexterityBuilder):
+    pass
+
+
+if getFSVersionTuple() > (5, ):
+    builder_registry.register('event', DXEventBuilder)
+else:
+    builder_registry.register('event', ATEventBuilder)
